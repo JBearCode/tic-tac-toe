0031b8975440e32d876ced14249323a78144d05b
JBearCode <joelbmoran@gmail.com>
2021-05-14 10:45:48 -0400

    Set up JS messages and game board

---
+++ script.js
@@ -0,0 +1,12 @@
+const gameStatusMessage = document.getElementById('game-status');
+
+let gameActive = true;
+
+let currentPlayer = "X";
+
+let gameBoard = ["","","","","","","","",""];
+
+const winningMessage = () => `${currentPlayer} wins!`
+const tieMessage = "It's a tie!";
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
