2e32a9033d78033591c128ebdd5a29bac7a7e16c
JBearCode <joelbmoran@gmail.com>
2021-05-14 13:39:20 -0400

    Build handleResult() for game result and changePlayer() to switch turns between x and o

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function handleClick(e) {
   }
 
   handlePlay(clickedSquare, clickedSquareNumber);
-  checkGameStatus();
+  handleResult();
 }
 
 function handlePlay(clickedElement, squareIndex) {
@@ -29,15 +29,53 @@ function handlePlay(clickedElement, squareIndex) {
   clickedElement.innerText = currentPlayer;
 }
 
-function changePlayer() {
+const winningCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
+function handleResult() {
+  let gameWon = false;
+  for (let i = 0; i <= 7; i++) {
+    let a = gameBoard[winningCombos[i][0]];
+    let b = gameBoard[winningCombos[i][1]];
+    let c = gameBoard[winningCombos[i][2]];
+    
+    if (!a || !b || !c) {
+      continue;
+    }
+    if (a === b && b === c) {
+      gameWon = true;
+      break;
+      }
+  }
+  if (gameWon) {
+      gameStatusMessage.innerText = winningMessage();
+      gameActive = false;
+      return
+  }
+  let tieGame = !gameBoard.includes("");
+  if (tieGame) {
+      gameStatusMessage.innerText = tieMessage;
+      gameActive = false;
+      return;
+  }
+  changePlayer();
+}
+
+function changePlayer() {
+  currentPlayer = currentPlayer=== "X" ? "O" : "X";
+  gameStatusMessage.innerText = currentPlayerTurn();
+  console.log('changePlayer() ran');
 }
 
 function restartGame() {
-
-}
-
-function checkGameStatus() {
 
 }
 
